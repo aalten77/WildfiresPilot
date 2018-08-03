3d09eec13fd343daa81b1a46ea6587de4e35c53c
ai-linhalten <ai-linh.alten@sjsu.edu>
2018-08-03 09:22:30 -0600

    adding more data

--- readjsonfromurl.py
+++ readjsonfromurl.py
@@ -1,9 +1,14 @@
 import requests, zipfile, io, json
 
-r = requests.get('https://github.com/aalten77/WildfiresPilot/raw/master/data/Tubbs/Fountaingrove/Fountaingrove.json.zip')
-z = zipfile.ZipFile(io.BytesIO(r.content))
-print z.printdir()
-file = z.open('Fountaingrove.json')
-js = json.load(file)
+def get_json_remote(link):
+    r = requests.get(link)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    print z.printdir()
+    file = z.open('Fountaingrove.json')
+    js = json.load(file)
 
-print len(js['features'])
+    return js
+
+def fetch_jsons():
+    js_list = []
+    remote_links = ['https://github.com/aalten77/WildfiresPilot/raw/master/data/Tubbs/Fountaingrove/Fountaingrove.json.zip', ]
