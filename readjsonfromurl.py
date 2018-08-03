158cfe93a1d8e6d9777208db786dcf3f51046176
ai-linhalten <ai-linh.alten@sjsu.edu>
2018-08-03 09:29:30 -0600

    read json from remote

--- readjsonfromurl.py
+++ readjsonfromurl.py
@@ -4,11 +4,19 @@ def get_json_remote(link):
     r = requests.get(link)
     z = zipfile.ZipFile(io.BytesIO(r.content))
     print z.printdir()
-    file = z.open('Fountaingrove.json')
+    file = z.open(link.split('/')[-1].replace('.zip',''))
     js = json.load(file)
 
     return js
 
 def fetch_jsons():
     js_list = []
-    remote_links = ['https://github.com/aalten77/WildfiresPilot/raw/master/data/Tubbs/Fountaingrove/Fountaingrove.json.zip', ]
+    remote_links = ['https://github.com/aalten77/WildfiresPilot/raw/master/data/Tubbs/Fountaingrove/Fountaingrove.json.zip',
+                    'https://github.com/aalten77/WildfiresPilot/raw/master/data/Tubbs/Fountaingrove/Fountaingrove_v2.json.zip',
+                    'https://github.com/aalten77/WildfiresPilot/raw/master/data/Tubbs/Fountaingrove/Fountaingrove_v3.json.zip']
+
+
+    for link in remote_links:
+        js_list.append(get_json_remote(link))
+
+    return js_list
